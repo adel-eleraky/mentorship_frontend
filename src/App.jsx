995692cae276172fd0822ebd05da19c2a5e8876b
el3raky <kameladel339@gmail.com>
2025-02-25 22:46:24 +0200

    feat: emit message socket

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import io from "socket.io-client"
+
+const socket = io("http://localhost:3000")
+
+function App() {
+
+  const [message, setMessage] = useState("");
+
+  const sendMessage = () => {
+    socket.emit("send_message", {sender_id: "67bdbc12abcca0e18a724d5e" , room: "67bdae230ce91c62982afd99",  content: message});
+    setMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      console.log(data)
+    })
+  }, [socket])
+  return (
+    <>
+
+      <div>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type your message here"
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </>
+  )
+}
+
+export default App
