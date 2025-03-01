bf82d4728580f9665bb06fbc05eba55c98b83d99
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-03-01 20:35:30 +0200

    add meeting route

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import "./App.css";
 import io from "socket.io-client";
 import Chat from "./components/Chat/Chat";
 import { BrowserRouter, Route, Routes } from "react-router";
+import CreateButton from "./components/CreateButton";
+import Meeting from "./pages/Meeting";
 
 const socket = io("http://localhost:3000");
 
 function App() {
-  
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CreateButton />} />
+          <Route path="meeting/:id" element={<Meeting />} />
+          <Route path="chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
