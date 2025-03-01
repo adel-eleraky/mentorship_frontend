a7d62e7e1a409e7e160bf3fd240f509f3ce96796
el3raky <kameladel339@gmail.com>
2025-03-01 20:26:55 +0200

    imported chat component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,21 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
+import Chat from "./components/Chat/Chat";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const socket = io("http://localhost:3000");
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  const sendMessage = () => {
-    socket.emit("send_message", {
-      sender_id: "67bdbc12abcca0e18a724d5e",
-      room: "67bdae230ce91c62982afd99",
-      content: message,
-    });
-    setMessage("");
-  };
-
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      console.log(data);
-    });
-  }, [socket]);
+  
   return (
     <>
-      <div>
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Type your message here"
-        />
-        <button onClick={sendMessage}>Send</button>
-      </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+      
     </>
   );
 }
