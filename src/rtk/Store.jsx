0c3fbc6a9c1091f07b9278940293d7889a353433
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-03-10 06:45:54 +0200

    added mentor reducer to store

--- src/rtk/Store.jsx
+++ src/rtk/Store.jsx
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import roomReducer from "./features/RoomSlice";
 import userReducer from "./features/userSlice";
-
+import mentorReducer from "./features/mentorSlice";
 
 const Store = configureStore({
-    reducer: {
-        room: roomReducer,
-        user: userReducer
-    }
-})
+  reducer: {
+    room: roomReducer,
+    user: userReducer,
+    mentor: mentorReducer,
+  },
+});
 
-export default Store
+export default Store;
