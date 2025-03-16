46807210a135d33757f1114e4a386c4ef1b9694b
adel eleraky <kameladel339@gmail.com>
2025-03-16 03:59:32 +0200

    imported auth slice to store

--- src/rtk/Store.jsx
+++ src/rtk/Store.jsx
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import roomReducer from "./features/RoomSlice";
 import userReducer from "./features/userSlice";
-import mentorReducer from "./features/mentorSlice"; import authReducer from "./features/authSlice";
+import mentorReducer from "./features/mentorSlice"; 
+import authReducer from "./features/authSlice";
 
 
 const Store = configureStore({
