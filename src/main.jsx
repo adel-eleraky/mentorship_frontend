e411c869d5d4e0cad623f6188a905ff237bd90e0
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-03-16 23:13:13 +0200

    wrapper browser router

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
