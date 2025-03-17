13975dd74a3e61a8635b30f2311fabee52510d8e
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-03-17 17:06:02 +0200

    add fetchMentorData  to mentorSlice

--- src/rtk/features/mentorSlice.jsx
+++ src/rtk/features/mentorSlice.jsx
@@ -15,6 +15,19 @@ export const updateMentorProfile = createAsyncThunk(
     }
   }
 );
+export const fetchMentorData = createAsyncThunk(
+  "mentor/getMentor",
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:3000/api/v1/mentors/67d5eb638678c21491e11a92"
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
 
 export const getMentorSessions = createAsyncThunk(
   "mentor/getSessions",
@@ -34,7 +47,7 @@ const initialState = {
   loading: false,
   status: "",
   message: "",
-  mentor: "",
+  mentor: null,
   sessions: [],
   errors: null,
 };
@@ -52,11 +65,13 @@ const mentorSlice = createSlice({
         state.status = action.payload.status;
         state.message = action.payload.message;
         state.mentor = action.payload.mentor;
+        state.loading = false;
       })
       .addCase(updateMentorProfile.rejected, (state, action) => {
         state.status = action.payload.status;
         state.message = action.payload.message;
         state.errors = action.payload.errors;
+        state.loading = false;
       })
       .addCase(getMentorSessions.pending, (state, action) => {
         state.loading = true;
@@ -71,6 +86,18 @@ const mentorSlice = createSlice({
         state.status = action.payload.status;
         state.message = action.payload.message;
         state.errors = action.payload.errors;
+      })
+      .addCase(fetchMentorData.pending, (state) => {
+        state.loading = true;
+        state.errors = null;
+      })
+      .addCase(fetchMentorData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.mentor = action.payload;
+      })
+      .addCase(fetchMentorData.rejected, (state, action) => {
+        state.loading = false;
+        state.errors = action.payload;
       });
   },
 });
