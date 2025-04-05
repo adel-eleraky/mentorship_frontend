0aa00ed646a01372846564d00c12f698659be23b
adel eleraky <kameladel339@gmail.com>
2025-04-05 06:23:16 +0200

    feat: finished verify email logic

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { verifyUser } from '../rtk/features/authSlice';
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router';
 import { CircularProgress, Typography, Box } from '@mui/material';
 
 function Verify() {
     const [isLoading, setIsLoading] = useState(true);
     const dispatch = useDispatch()
-    const {token} = useParams()
+    const { token } = useParams()
 
     useEffect(() => {
         dispatch(verifyUser(token))
@@ -15,11 +15,11 @@ function Verify() {
     }, [])
 
     return (
-        <Box 
-            display="flex" 
-            justifyContent="center" 
-            alignItems="center" 
-            height="100vh" 
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            // height="100vh"
             fontFamily="Arial, sans-serif"
         >
             {isLoading ? (
@@ -30,9 +30,18 @@ function Verify() {
                     </Typography>
                 </>
             ) : (
-                <Typography variant="h6" color="success.main">
-                    Verification complete!
-                </Typography>
+                // <Typography variant="h6" color="success.main">
+                //     Verification complete!
+                // </Typography>
+                <div className='d-flex justify-content-center align-content-center my-5 ' >
+                    <div className='p-5 shadow-lg rounded' style={{ width: "fit-content" }}>
+                        <h4>
+                            Verified Email successfully
+                        </h4>
+                        <i className="fa-solid fa-circle-check d-block mx-auto fs-1 my-3" style={{ color: "#2be34a", width: "fit-content" }}></i>
+                        <Link to="/user" className='btn btn-primary d-block mx-auto'> Go to Dashboard </Link>
+                    </div>
+                </div>
             )}
         </Box>
     )
