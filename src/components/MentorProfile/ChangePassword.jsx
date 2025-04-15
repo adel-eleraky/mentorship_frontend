a08f7a16a79c0b2587d4a42be5c1e41c9925f22c
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-04-15 05:43:24 +0200

    change password ui

--- src/components/MentorProfile/ChangePassword.jsx
+++ src/components/MentorProfile/ChangePassword.jsx
@@ -4,9 +4,15 @@ import * as Yup from "yup";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 export default function ChangePassword({ person }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [showOldPassword, setShowOldPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   // Validation schema
   const passwordSchema = Yup.object({
@@ -68,125 +74,166 @@ export default function ChangePassword({ person }) {
 
   return (
     <>
-         <div
-            className="profile-header mb-1 p-3 bg-gradient rounded-1 "
-            style={{
-              background: "linear-gradient(135deg, #f8f9fa 0%, #e9ecef 100%)",
-              borderBottom: "3px solid white",
-            }}
-          >
-            <div className="row align-items-center">
-              <div className="col-lg-8 col-md-7">
-                <h2 className="display-6 fw-bold  second-color mb-3">
-                Change Password
-                </h2>
-                <p className="text-muted lead mb-0">
-                Dashboard / <span className="frist-color ">Change Password </span>
-                </p>
-              </div>
-              <div className="col-lg-4 col-md-5 text-md-end mt-4 mt-md-0">
-              </div>
-            </div>
+      <div
+        className="profile-header mb-1 p-3 bg-gradient rounded-1 "
+        style={{
+          background: "linear-gradient(135deg, #f8f9fa 0%, #e9ecef 100%)",
+          borderBottom: "3px solid white",
+        }}
+      >
+        <div className="row align-items-center">
+          <div className="col-lg-8 col-md-7">
+            <h2 className="display-6 fw-bold  second-color mb-3">
+              Change Password
+            </h2>
+            <p className="text-muted lead mb-0">
+              Dashboard / <span className="frist-color ">Change Password </span>
+            </p>
           </div>
-
-        <div className=" card-border ">
-      <div className="card-body">
-      
-
-        <Formik
-          initialValues={initialValues}
-          validationSchema={passwordSchema}
-          onSubmit={handleSubmit}
-        >
-          {({ isSubmitting, errors, touched }) => (
-            <Form>
-              <div className="mb-3">
-                <label htmlFor="oldPassword" className="form-label second-color">
-                  Current Password
-                </label>
-                <Field
-                  type="password"
-                  className={`form-control ${
-                    errors.oldPassword && touched.oldPassword
-                      ? "is-invalid"
-                      : ""
-                  }`}
-                  id="oldPassword"
-                  name="oldPassword"
-                />
-                <ErrorMessage
-                  name="oldPassword"
-                  component="div"
-                  className="text-danger"
-                />
-              </div>
-
-              <div className="mb-3">
-                <label htmlFor="newPassword" className="form-label second-color">
-                  New Password
-                </label>
-                <Field
-                  type="password"
-                  className={`form-control ${
-                    errors.newPassword && touched.newPassword
-                      ? "is-invalid"
-                      : ""
-                  }`}
-                  id="newPassword"
-                  name="newPassword"
-                />
-                <ErrorMessage
-                  name="newPassword"
-                  component="div"
-                  className="text-danger"
-                />
-              </div>
-
-              <div className="mb-3">
-                <label htmlFor="confirmedPassword" className="form-label second-color">
-                  Confirm New Password
-                </label>
-                <Field
-                  type="password"
-                  className={`form-control ${
-                    errors.confirmedPassword && touched.confirmedPassword
-                      ? "is-invalid"
-                      : ""
-                  }`}
-                  id="confirmedPassword"
-                  name="confirmedPassword"
-                />
-                <ErrorMessage
-                  name="confirmedPassword"
-                  component="div"
-                  className="text-danger"
-                />
-              </div>
-
-              <button
-                type="submit"
-                className="btn  bg-second-color"
-                disabled={isLoading}
-              >
-                {isLoading ? (
-                  <>
-                    <span
-                      className="spinner-border spinner-border-sm me-2"
-                      role="status"
-                      aria-hidden="true"
-                    ></span>
-                    Updating...
-                  </>
-                ) : (
-                  "Update Password"
-                )}
-              </button>
-            </Form>
-          )}
-        </Formik>
+          <div className="col-lg-4 col-md-5 text-md-end mt-4 mt-md-0"></div>
+        </div>
       </div>
-    </div>
-    </>
 
+      <div className=" card-border ">
+        <div className="card-body">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={passwordSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting, errors, touched }) => (
+              <Form>
+                <div className="mb-3">
+                  <Field name="oldPassword">
+                    {({ field, meta }) => (
+                      <TextField
+                        {...field}
+                        label="Current Password"
+                        variant="outlined"
+                        fullWidth
+                        type={showOldPassword ? "text" : "password"}
+                        error={meta.touched && !!meta.error}
+                        helperText={
+                          meta.touched && meta.error ? meta.error : ""
+                        }
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <IconButton
+                                onClick={() =>
+                                  setShowOldPassword(!showOldPassword)
+                                }
+                                edge="end"
+                              >
+                                {showOldPassword ? (
+                                  <VisibilityOffIcon />
+                                ) : (
+                                  <VisibilityIcon />
+                                )}
+                              </IconButton>
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
+
+                <div className="mb-3">
+                  <Field name="newPassword">
+                    {({ field, meta }) => (
+                      <TextField
+                        {...field}
+                        label="New Password"
+                        variant="outlined"
+                        fullWidth
+                        type={showNewPassword ? "text" : "password"}
+                        error={meta.touched && !!meta.error}
+                        helperText={
+                          meta.touched && meta.error ? meta.error : ""
+                        }
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <IconButton
+                                onClick={() =>
+                                  setShowNewPassword(!showNewPassword)
+                                }
+                                edge="end"
+                              >
+                                {showNewPassword ? (
+                                  <VisibilityOffIcon />
+                                ) : (
+                                  <VisibilityIcon />
+                                )}
+                              </IconButton>
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
+
+                <div className="mb-3">
+                  <Field name="confirmedPassword">
+                    {({ field, meta }) => (
+                      <TextField
+                        {...field}
+                        label="Confirm New Password"
+                        variant="outlined"
+                        fullWidth
+                        type={showConfirmPassword ? "text" : "password"}
+                        error={meta.touched && !!meta.error}
+                        helperText={
+                          meta.touched && meta.error ? meta.error : ""
+                        }
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <IconButton
+                                onClick={() =>
+                                  setShowConfirmPassword(!showConfirmPassword)
+                                }
+                                edge="end"
+                              >
+                                {showConfirmPassword ? (
+                                  <VisibilityOffIcon />
+                                ) : (
+                                  <VisibilityIcon />
+                                )}
+                              </IconButton>
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
+
+                <button
+                  type="submit"
+                  className="btn bg-second-color"
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <>
+                      <span
+                        className="spinner-border spinner-border-sm me-2"
+                        role="status"
+                        aria-hidden="true"
+                      ></span>
+                      Updating...
+                    </>
+                  ) : (
+                    "Update Password"
+                  )}
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </>
   );
 }
