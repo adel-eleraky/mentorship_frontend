e0fb43fe08e8888ae7f1adfa183a30127d5dc8f9
heba sabri <heba.sabri.183@gmail.com>
2025-04-16 12:48:35 +0200

    some edit at home

--- src/components/MentorCardSmall/MentorCardSmall.jsx
+++ src/components/MentorCardSmall/MentorCardSmall.jsx
@@ -4,12 +4,14 @@ import profileImg from "../../assets/profile-img.png";
 export default function MentorCardSmall({ mentor }) {
   return (
     <div className="card border-0 p-3">
+      <div className="d-flex justify-content-center align-items-center">
       <img
         src={`http://localhost:3000/img/users/${mentor?.image}`}
         alt={mentor.name}
-        className="card-img-top rounded"
-        style={{ height: "200px", objectFit: "contain" }}
+        className="card-img-top"
+        style={{ width: "200px", height: "200px", borderRadius: "50px", objectFit: "contain" }}
       />
+      </div>
       <div className="card-body text-center">
         {/* <h5 className="card-title fw-bold">{mentor.name}</h5> */}
         <h5 className="card-title fw-bold second-color">{mentor.name}</h5>
